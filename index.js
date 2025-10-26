619c7ad173d9942fa7ceb753efb790424da49e5e
ZikaZK <zikav2000v@gmail.com>
2025-10-26 13:46:55 -0300

    Atividade 01

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     const matricula = parseInt(req.query.matricula);
     var guarda = 0;
     res.write(`
-        <DOCTYPE html>
+        <!DOCTYPE html>
         <html lang="pt-br">
         <head>
             <meta charset="UTF-8">
@@ -27,11 +27,11 @@ app.get('/', (req, res) => {
         <p>Informe na URL no subdiretório de "reajuste" os seguintes dados: idade, sexo (F ou M), salarioBase, anoContratacao e matricula</p>
     `);
 
-    if(!idade.isInteger() || idade <= 16){
+    if(!Number.isInteger(idade) || idade <= 16){
         res.write('<p>A idade precisa ser acima de 16 anos</p>');
         guarda = 1;
     }
-    if(sexo.toUpperCase() != 'F' || sexo.toUpperCase() != 'M'){
+    if(sexo.toUpperCase() != 'F' && sexo.toUpperCase() != 'M'){
         res.write('<p>O sexo está inválido</p>');
         guarda = 1;
     }
@@ -53,7 +53,7 @@ app.get('/', (req, res) => {
         let anosEmpresa = 2025 - anoContratacao;
         res.write(`
             <h2>Informações:</h2><br><br>
-            <p>Nome: ${nome}</p>
+            <p>Idade: ${idade}</p>
             <p>Sexo: ${sexo}</p>
             <p>Salário base: R$${salarioBase.toFixed()}</p>
             <p>Ano de contratação: ${anoContratacao}</p>
@@ -61,7 +61,7 @@ app.get('/', (req, res) => {
             <br>
         `);
         
-        if(idade > 16 || idade <= 39){
+        if(idade > 16 && idade <= 39){
             if(sexo.toUpperCase() == 'M'){
                 salarioNovo = salarioBase * 1.1;
                 if(anosEmpresa <= 10){
@@ -77,7 +77,7 @@ app.get('/', (req, res) => {
                     salarioNovo += 16.0;
                 }
             }
-        } else if(idade > 39 || idade <= 69){
+        } else if(idade > 39 && idade <= 69){
             if(sexo.toUpperCase() == 'M'){
                 salarioNovo = salarioBase * 1.08;
                 if(anosEmpresa <= 10){
@@ -118,117 +118,5 @@ app.get('/', (req, res) => {
         </body>
         </html>    
     `);
-});
-
-app.get('/reajuste', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    const idade = parseInt(req.query.idade);
-    const sexo = req.query.sexo;
-    const salarioBase = parseFloat(req.query.salarioBase);
-    const anoContratacao = parseInt(req.query.anoContratacao);
-    const matricula = parseInt(req.query.matricula);
-    let guarda = 0;
-    res.write(`
-        <DOCTYPE html>
-        <html lang="pt-br">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Atividade 1</title>
-        </head>
-        <body>
-    `);
-
-    if(!idade.isInteger() || idade <= 16){
-        res.write('<p>A idade precisa ser acima de 16 anos</p>');
-        guarda = 1;
-    }
-    if(sexo.toUpperCase() != 'F' || sexo.toUpperCase() != 'M'){
-        res.write('<p>O sexo está inválido</p>');
-        guarda = 1;
-    }
-    if(!(typeof salarioBase === 'number' && Number.isFinite(salarioBase) && !Number.isInteger(salarioBase)) || salarioBase < 0){
-        res.write('<p>O salário base precisa ser um número real positivo</p>');
-        guarda = 1;
-    }
-    if(!anoContratacao.isInteger() || anoContratacao <= 1960){
-        res.write('<p>O ano de contratação precisa ser acima de 1960</p>');
-        guarda = 1;
-    }
-    if(!matricula.isInteger() || matricula < 0){
-        res.write('<p>O número de matrícula precisa ser um inteiro maior que 0</p>');
-        guarda = 1;
-    }
-
-    if(guarda != 1){
-        let salarioNovo;
-        let anosEmpresa = 2025 - anoContratacao;
-        res.write(`
-            <h2>Informações:</h2><br><br>
-            <p>Nome: ${nome}</p>
-            <p>Sexo: ${sexo}</p>
-            <p>Salário base: R$${salarioBase.toFixed()}</p>
-            <p>Ano de contratação: ${anoContratacao}</p>
-            <p>Número de matrícula: ${matricula}</p>
-            <br>
-        `);
-        
-        if(idade > 16 || idade <= 39){
-            if(sexo.toUpperCase() == 'M'){
-                salarioNovo = salarioBase * 1.1;
-                if(anosEmpresa <= 10){
-                    salarioNovo -= 10.0;
-                } else {
-                    salarioNovo += 17.0;
-                }
-            } else if(sexo.toUpperCase() == 'F'){
-                salarioNovo = salarioBase * 1.08;
-                if(anosEmpresa <= 10){
-                    salarioNovo -= 11.0;
-                } else {
-                    salarioNovo += 16.0;
-                }
-            }
-        } else if(idade > 39 || idade <= 69){
-            if(sexo.toUpperCase() == 'M'){
-                salarioNovo = salarioBase * 1.08;
-                if(anosEmpresa <= 10){
-                    salarioNovo -= 5.0;
-                } else {
-                    salarioNovo += 15.0;
-                }
-            } else if(sexo.toUpperCase() == 'F'){
-                salarioNovo = salarioBase * 1.1;
-                if(anosEmpresa <= 10){
-                    salarioNovo -= 7.0;
-                } else {
-                    salarioNovo += 14.0;
-                }
-            }
-        } else if(idade > 69){
-            if(sexo.toUpperCase() == 'M'){
-                salarioNovo = salarioBase * 1.15;
-                if(anosEmpresa <= 10){
-                    salarioNovo -= 15.0;
-                } else {
-                    salarioNovo += 13.0;
-                }
-            } else if(sexo.toUpperCase() == 'F'){
-                salarioNovo = salarioBase * 1.17;
-                if(anosEmpresa <= 10){
-                    salarioNovo -= 17.0;
-                } else {
-                    salarioNovo += 12.0;
-                }
-            }
-        }
-
-        res.write(`<p>Novo salário reajustado: R$${salarioNovo.toFixed(2)}`);
-    }
-
-    res.write(`
-        </body>
-        </html>    
-    `);
+    res.end();
 });
